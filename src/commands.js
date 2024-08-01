4744947374b5b6c7115887be2f801c3c93c50eab
Pedro Lugon <pedromotta20@gmail.com>
2024-08-01 14:58:26 -0300

    refac(bot): change file from bot to commands

--- src/commands.js
+++ src/commands.js
@@ -13,13 +13,9 @@ const { Routes } = require("discord-api-types/v10");
   const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
   try {
-    // console.log("Started refreshing application (/) commands.");
-
     await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
       body: commands,
     });
-
-    // console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
