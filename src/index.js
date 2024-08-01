cff9a6371e914f2cf7b99fd5823fff263e72054b
Pedro Lugon <pedromotta20@gmail.com>
2024-08-01 14:43:46 -0300

    refac(bot): change structure a little bit to start task

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { Client, IntentsBitField } = require("discord.js");
+require("dotenv").config();
+
+const GatewayIntentBits = require("discord.js").GatewayIntentBits;
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
