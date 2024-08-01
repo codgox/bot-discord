64308bd3635a4177761df763691585a8ffdfe1f5
Pedro Lugon <pedromotta20@gmail.com>
2024-08-01 15:25:29 -0300

    feat(schedule): add node-schedule to handle cron

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 require("dotenv").config();
 const { Client } = require("discord.js");
 const GatewayIntentBits = require("discord.js").GatewayIntentBits;
+const schedule = require("node-schedule");
+const { SendMessageInChannel } = require("./send-message-in-channel");
+const timeZone = "America/Sao_Paulo";
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  // console.log(`Logged in as ${client.user.tag}!`);
+
+  schedule.scheduleJob({ hour: 13, minute: 30, tz: timeZone }, () => {
+    SendMessageInChannel(client, process.env.DISCORD_CHANNEL_ID, "Teste");
+  });
+
+  schedule.scheduleJob({ hour: 15, minute: 23, tz: timeZone }, () => {
+    SendMessageInChannel(client, process.env.DISCORD_CHANNEL_ID, "Teste");
+  });
+
+  schedule.scheduleJob({ hour: 17, minute: 30, tz: timeZone }, () => {
+    SendMessageInChannel(client, process.env.DISCORD_CHANNEL_ID, "Teste");
+  });
 });
 
 client.on("interactionCreate", async (interaction) => {
